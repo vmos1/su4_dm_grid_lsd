ca368fa9e6be401cc19db7decc917c6150a62472
Venkitesh <vayyar@bu.edu>
2023-01-10 15:42:56 -0500

    Modified makefile for tioga. modified submit file for dwhsdm

--- dm_tests/run_tioga/submit_2_dwhsdm.sh
+++ dm_tests/run_tioga/submit_2_dwhsdm.sh
@@ -2,9 +2,9 @@
 # Begin LSF Directives
 #SBATCH -A lsd
 #SBATCH -t 00:10:00
-#SBATCH -J hsdm2nodes
-#SBATCH -o hsdm2nodes.%J
-#SBATCH -e hsdm2nodes.%J
+#SBATCH -J sdm4nodes
+#SBATCH -o sdm4nodes.%J
+#SBATCH -e sdm4nodes.%J
 #SBATCH -N 2
 #SBATCH -n 8
 #SBATCH --exclusive
@@ -20,24 +20,20 @@ export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
 export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=16384
 export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_SMP_SINGLE_COPY_MODE=CMA
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
 export MPICH_OFI_NIC_POLICY=GPU
 export OPT="--comms-concurrent --comms-overlap "
-
 
 source $GRID_DIR/setup_env.sh
 export TSAN_OPTIONS='ignore_noninstrumented_modules=1'
 export LD_LIBRARY_PATH
 
-BETA=10.0
-M_F=0.1
-N_S=4
-PARAMS=" --accelerator-threads 8 --grid 64.64.32.32 --mpi 2.2.2.1 --comms-sequential --shm 2048 --shm-mpi 1"
+BETA=11.0
+M_F=0.6443 # kappa=0.1490
+Ls=4
 
-APP="$RUN_DIR/dm_tests/build/dweofa_HSDM --grid 16.16.16.16 --mpi 2.2.2.2 --shm 2048 --shm-force-mpi 1 --device-mem 5000 $OPT $BETA $M_F $N_S"
-
-#jsrun --nrs 4 -a4 -g4 -r1 -c40 -dpacked -b packed:10 --latency_priority gpu-cpu --smpiargs=-gpu $APP > SDM.4node
-srun --gpus-per-task 1 -n16 $BIND $APP > HSDM.2node
+APP="$RUN_DIR/dm_tests/build/dweofa_HSDM --grid 8.8.8.16 --mpi 2.2.2.2 --shm 2048 --shm-force-mpi 1 --device-mem 5000 --Trajectories 200 --Thermalizations 10 $OPT $BETA $M_F $Ls"
+srun --gpus-per-task 1 -n16 $BIND $APP > SDM.4node
 
 echo "--end " `date` `date +%s`
 
