4e7f59e4eef6486849208b1920e27627b1c5941a
Venkitesh <vayyar@bu.edu>
2023-02-06 14:36:48 -0500

    updated crusher builds docs

--- grid_build/grid_crusher/build_grid_crusher.sh
+++ grid_build/grid_crusher/build_grid_crusher.sh
@@ -1,8 +1,9 @@
 
 # sites: Lime, MPFR place in $HOME/prefix
 ##################
-export prefix=/gpfs/alpine/lgt104/proj-shared/ayyar/LSD_projects/build_LSD_grid/install_nov2_2022/grid_prefix
-export grid=/gpfs/alpine/lgt104/proj-shared/ayyar/LSD_projects/build_LSD_grid/install_nov2_2022
+export grid=$GRID_DIR
+export prefix=$GRID_DIR/grid_prefix
+
 mkdir -p $prefix
 mkdir -p $grid
 echo GRID $grid
@@ -22,11 +23,15 @@ make all install 2>&1 | tee op_lime.out
 #MPFR
 ##################
 cd $prefix
-wget https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.gz
-tar xvzf mpfr-4.1.0.tar.gz
-cd mpfr-4.1.0
-module load gmp
-./configure --prefix $prefix --with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/cce-14.0.0/gmp-6.2.1-gw47mfuvanwrvrh3rj6qrjz6hpey7423/
+wget https://www.mpfr.org/mpfr-current/mpfr-4.2.0.tar.gz
+tar xvzf mpfr-4.2.0.tar.gz
+cd mpfr-4.2.0
+echo $OLCF_GMP_ROOT
+
+#./configure --prefix $prefix --with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/cce-14.0.0/gmp-6.2.1-gw47mfuvanwrvrh3rj6qrjz6hpey7423/
+#./configure --prefix $prefix --with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen2/gcc-11.2.0/gmp-6.2.1-idotvdzakdbt5mdfbxiffbln672cifcz
+./configure --prefix $prefix --with-gmp=$OLCF_GMP_ROOT
+
 make all install 2>&1 | tee op_mpfr.out
 
 ##################
@@ -46,12 +51,9 @@ cd $grid/
 mkdir -p install/build-Nc4
 cd install/build-Nc4
 
-../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink \
---enable-accelerator=hip  --enable-gen-simd-width=64  --enable-simd=GPU --enable-Nc=4 \
---with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/cce-14.0.0/gmp-6.2.1-gw47mfuvanwrvrh3rj6q --with-mpfr=/opt/cray/pe/gcc/mpfr/3.1.4/  \
---disable-fermion-reps   --prefix=$prefix \
-CXX=hipcc MPICXX=mpicxx \
-CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include  -g"  LDFLAGS=" -L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_hsa -lamdhip64  -g -L/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/gperftools-2.9.1-72ubwtuc5wcz2meqltbfdb76epufgzo2/lib -ltcmalloc" 
+#../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64 --enable-simd=GPU --enable-Nc=4  --with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen2/gcc-11.2.0/gmp-6.2.1-idotvdzakdbt5mdfbxiffbln672cifcz --with-mpfr=/opt/cray/pe/gcc/mpfr/3.1.4/ --with-fftw=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/fftw-3.3.9-3hxcnpeqmrvidsb3erk36wi75kexaznd  --disable-fermion-reps  --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include  -g"
+
+../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64 --enable-simd=GPU --enable-Nc=4  --with-gmp=$OLCF_GMP_ROOT --with-fftw=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/fftw-3.3.9-3hxcnpeqmrvidsb3erk36wi75kexaznd  --disable-fermion-reps  --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include -g" LDFLAGS="-L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lamdhip64"
 
 make -j 14 2>&1 | tee op_grid.out
 make install
--- grid_build/grid_crusher/grid_benchmarks_tests/run_benchmark2_2nodes_dwf_fp32.sh
+++ grid_build/grid_crusher/grid_benchmarks_tests/run_benchmark2_2nodes_dwf_fp32.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
-# Begin LSF Directives
+
 #SBATCH -A lgt104_crusher
-#SBATCH -t 00:10:00
-#SBATCH -J Benchmark_dwf_fp32_2nodes
-#SBATCH -o Benchmark_dwf_fp32_2nodes.%J
-#SBATCH -e Benchmark_dwf_fp32_2nodes.%J
+#SBATCH -t 00:30:00
+#SBATCH -J Benchmark_dwf_fp32_1node
+#SBATCH -o Benchmark_dwf_fp32_1node.%J
+#SBATCH -e Benchmark_dwf_fp32_1node.%J
 #SBATCH -N 2
 #SBATCH -n 8
-#SBATCH --exclusive
-#SBATCH --gpu-bind=map_gpu:0,1,2,3,7,6,5,4
-#SBATCH -c 8
-#SBATCH --threads-per-core=1
-export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
+#SBATCH -C nvme
+#SBATCH --cpus-per-task=7
+#SBATCH --ntasks-per-node=8
 
 export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=16384
 export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_SMP_SINGLE_COPY_MODE=CMA
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=7
 export MPICH_OFI_NIC_POLICY=GPU
 
-SETUP_DIR=/gpfs/alpine/lgt104/proj-shared/ayyar/LSD_projects/build_LSD_grid/install_nov2_2022
-echo $SETUP_DIR
-source $SETUP_DIR/setup_env.sh
+# New vars
+rm -f ./core
+ulimit -c unlimited
+export OMP_NUM_THREADS=7
+export OMP_PROC_BIND=spread
+MASK_0="0x00fe000000000000"
+MASK_1="0xfe00000000000000"
+MASK_2="0x0000000000fe0000"
+MASK_3="0x00000000fe000000"
+MASK_4="0x00000000000000fe"
+MASK_5="0x000000000000fe00"
+MASK_6="0x000000fe00000000"
+MASK_7="0x0000fe0000000000"
+MEMBIND="--mem-bind=map_mem:3,3,1,1,0,0,2,2"
+CPU_MASK="--cpu-bind=mask_cpu:${MASK_0},${MASK_1},${MASK_2},${MASK_3},${MASK_4},${MASK_5},${MASK_6},${MASK_7}"
+
+#cmd="srun -n $((nodes*8)) -N $nodes --ntasks-per-node=8 --cpus-per-task=7 ${CPU_MASK} ${MEMBIND} ${APP}"
+
+source $GRID_DIR/setup_env.sh
 PARAMS=" --accelerator-threads 8 --grid 64.64.32.32 --mpi 2.2.2.2 --comms-sequential --shm 2048 --shm-mpi 1"
-srun --gpus-per-task 1 -n16 $BIND ./wrap.sh  $SETUP_DIR/install/build-Nc4/benchmarks/Benchmark_dwf_fp32 $PARAMS
+srun --gpus-per-task 1 -n16 $BIND ./wrap.sh  $GRID_DIR/install/build-Nc4/benchmarks/Benchmark_dwf_fp32 $PARAMS ${CPU_MASK} ${MEMBIND} ${APP}
