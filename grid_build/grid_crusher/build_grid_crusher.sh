265b3a8f7ee3459695b20322e498a46fe1bd2c33
Venkitesh <vayyar@bu.edu>
2023-02-07 10:13:56 -0500

    fixed bug in crusher build script

--- grid_build/grid_crusher/build_grid_crusher.sh
+++ grid_build/grid_crusher/build_grid_crusher.sh
@@ -51,9 +51,7 @@ cd $grid/
 mkdir -p install/build-Nc4
 cd install/build-Nc4
 
-#../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64 --enable-simd=GPU --enable-Nc=4  --with-gmp=/sw/crusher/spack-envs/base/opt/cray-sles15-zen2/gcc-11.2.0/gmp-6.2.1-idotvdzakdbt5mdfbxiffbln672cifcz --with-mpfr=/opt/cray/pe/gcc/mpfr/3.1.4/ --with-fftw=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/fftw-3.3.9-3hxcnpeqmrvidsb3erk36wi75kexaznd  --disable-fermion-reps  --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include  -g"
-
-../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64 --enable-simd=GPU --enable-Nc=4  --with-gmp=$OLCF_GMP_ROOT --with-fftw=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/fftw-3.3.9-3hxcnpeqmrvidsb3erk36wi75kexaznd  --disable-fermion-reps  --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include -g" LDFLAGS="-L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lamdhip64"
+../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64 --enable-simd=GPU --enable-Nc=4 --with-mpfr=/opt/cray/pe/gcc/mpfr/3.1.4 --with-gmp=$OLCF_GMP_ROOT --with-fftw=/sw/crusher/spack-envs/base/opt/cray-sles15-zen3/gcc-11.2.0/fftw-3.3.9-3hxcnpeqmrvidsb3erk36wi75kexaznd  --disable-fermion-reps  --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include -g" LDFLAGS="-L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lamdhip64"
 
 make -j 14 2>&1 | tee op_grid.out
 make install
