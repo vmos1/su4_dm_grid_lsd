2cf471fbcb0f082e85695513b7f0bd179beb82e0
Venkitesh <vayyar@bu.edu>
2023-05-08 11:41:24 -0400

    Added new script to generate folder and script for run for different input parameters

--- dm_tests/run_tioga/submit_2_dwhsdm.sh
+++ dm_tests/run_tioga/submit_2_dwhsdm.sh
@@ -2,9 +2,9 @@
 # Begin LSF Directives
 #SBATCH -A latticgc
 #SBATCH -t 00:10:00
-#SBATCH -J sdm8nodes
-#SBATCH -o sdm8nodes.%J
-#SBATCH -e sdm8nodes.%J
+#SBATCH -J hsdm8nodes
+#SBATCH -o hsdm8nodes.%J
+#SBATCH -e hsdm8nodes.%J
 #SBATCH -N 8
 #SBATCH -n 8
 #SBATCH --exclusive
@@ -32,7 +32,7 @@ Ls=4
 traj_l=1
 md_steps=10
 BETA=11.0
-M_F=0.6443 # kappa=0.1490
+M_F=0.6443
 
 APP="$RUN_DIR/dm_tests/build/dweofa_HSDM --grid 16.16.16.8 --mpi 4.4.2.2 --shm 2048 --shm-force-mpi 1 --device-mem 5000 --Trajectories 200 --Thermalizations 10 $OPT $Ls $traj_l $md_steps $BETA $M_F"
 #srun --gpus-per-task 1 -n64 $BIND $APP > HSDM.out
