db8ed8dd055cbba0ed09e9c46aa6e624a7f4b1bb
Venkitesh <vayyar@bu.edu>
2023-06-08 10:15:18 -0400

    Fixed bug for benchmark scripts on Tioga. Added analysis code for scaling runs

--- grid_build/grid_tioga/grid_benchmarks_tests/run_benchmark1_1node_dwf_fp32.sh
+++ grid_build/grid_tioga/grid_benchmarks_tests/run_benchmark1_1node_dwf_fp32.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Begin LSF Directives
-#SBATCH -A lsd
+#SBATCH -A latticgc
 #SBATCH -t 00:10:00
 #SBATCH -J Benchmark_dwf_fp32_1node
 #SBATCH -o Benchmark_dwf_fp32_1node.%J
@@ -10,8 +10,10 @@
 #SBATCH --exclusive
 #SBATCH --gpu-bind=map_gpu:0,1,2,3,7,6,5,4
 #SBATCH -c 8
-#SBATCH --threads-per-core=1
-export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
+###SBATCH --threads-per-core=1
+
+
+##export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
 
 export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=16384
 export MPICH_GPU_SUPPORT_ENABLED=1
@@ -20,6 +22,11 @@ export OMP_NUM_THREADS=8
 export MPICH_OFI_NIC_POLICY=GPU
 
 source $GRID_DIR/setup_env.sh
-PARAMS=" --accelerator-threads 8 --grid 64.64.32.32 --mpi 2.2.2.1 --comms-sequential --shm 2048 --shm-mpi 1"
-srun --gpus-per-task 1 -n8 $BIND ./wrap.sh  $GRID_DIR/install/build-Nc4/benchmarks/Benchmark_dwf_fp32 $PARAMS
+export TSAN_OPTIONS='ignore_noninstrumented_modules=1'
+export LD_LIBRARY_PATH
 
+PARAMS=" --grid 64.64.32.32 --mpi 2.2.2.1 --comms-sequential --shm 2048 --shm-mpi 1"
+APP="$GRID_DIR/install/build-Nc4/benchmarks/Benchmark_dwf_fp32 $PARAMS"
+##srun -n8 $BIND ./wrap $APP > run1_1node.out
+srun -n8 $APP > run1_1node.out
+echo "--end " `date` `date +%s`
--- grid_build/grid_tioga/grid_benchmarks_tests/run_benchmark3_dwf_ITT.sh
+++ grid_build/grid_tioga/grid_benchmarks_tests/run_benchmark3_dwf_ITT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Begin LSF Directives
-#SBATCH -A lsd
+#SBATCH -A latticgc
 #SBATCH -t 00:10:00
 #SBATCH -J Benchmark_ITT
 #SBATCH -o Benchmark_ITT.%J
@@ -10,8 +10,8 @@
 #SBATCH --exclusive
 #SBATCH --gpu-bind=map_gpu:0,1,2,3,7,6,5,4
 #SBATCH -c 8
-#SBATCH --threads-per-core=1
-export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
+###SBATCH --threads-per-core=1
+###export BIND="--cpu-bind=verbose,map_ldom:3,3,1,1,2,2,0,0"
 
 export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=16384
 export MPICH_GPU_SUPPORT_ENABLED=1
@@ -20,7 +20,14 @@ export OMP_NUM_THREADS=8
 export MPICH_OFI_NIC_POLICY=GPU
 
 source $GRID_DIR/setup_env.sh
+export TSAN_OPTIONS='ignore_noninstrumented_modules=1'
+export LD_LIBRARY_PATH
 
-PARAMS=" --accelerator-threads 8 --grid 64.64.32.32 --mpi 2.2.2.2 --comms-sequential --shm 2048 --shm-mpi 1"
-srun --gpus-per-task 1 -n16 $BIND ./wrap.sh  $GRID_DIR/install/build-Nc4/benchmarks/Benchmark_ITT $PARAMS
-
+## PARAMS=" --accelerator-threads 8 --grid 64.64.32.32 --mpi 2.2.2.2 --comms-sequential --shm 2048 --shm-mpi 1"
+PARAMS=" --grid 64.64.32.32 --mpi 2.2.2.2 --comms-concurrent --comms-overlap --shm 2048 --shm-mpi 1"
+APP="$GRID_DIR/install/build-Nc4/benchmarks/Benchmark_ITT $PARAMS"
+echo $APP
+##srun --gpus-per-task 1 -n16 $BIND ./wrap.sh  $GRID_DIR/install/build-Nc4/benchmarks/Benchmark_ITT $PARAMS
+##srun -n16 $BIND ./wrap $APP > run3_2node.out
+srun -n16 $APP > run3_2node.out
+echo "--end " `date` `date +%s`
