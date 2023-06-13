8b96bdb6a9c9313faa2fbdc255e9e868419a51f0
Venkitesh <vayyar@bu.edu>
2023-06-13 06:43:08 -0400

    Modified directory structure. Added new code for building hadrons

---
+++ hadrons_build/hadrons_tioga/build_hadrons_tioga.sh
@@ -0,0 +1,19 @@
+##################
+export grid=$GRID_DIR
+export prefix=$GRID_DIR/grid_prefix
+export build=$grid/hadrons_build2
+#################
+cd $grid
+
+source setup_env.sh
+git clone git@github.com:aportelli/Hadrons.git
+cd Hadrons
+./bootstrap.sh
+
+mkdir $build
+cd $build
+
+../Hadrons/configure --prefix=$prefix  --with-grid=$prefix
+
+make -j 14 
+make install
