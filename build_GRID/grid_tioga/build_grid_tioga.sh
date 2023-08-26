ce242003091f3cb5b226d76cb644e991d2b5ed60
Venkitesh <vayyar@bu.edu>
2023-08-25 21:44:45 -0400

    fixes for grid and hadrons on tioga

--- build_GRID/grid_tioga/build_grid_tioga.sh
+++ build_GRID/grid_tioga/build_grid_tioga.sh
@@ -28,8 +28,13 @@ make all install 2>&1 | tee op_lime.out
 
 cd $grid
 source setup_env.sh
+module list 
+
 git clone https://paboyle@github.com/paboyle/Grid
 cd Grid
+git checkout -b june15_2023 e3e1cc19620b8ee9834dfb35491ff6c36857d52c
+#git checkout -b may11_2023 876c8f4478886479bcd1e505e5481bf34afb8958
+#git checkout -b june15_2023 d72e914cf0ed7be6749f13dc307fa0dceaa1add2
 ./bootstrap.sh
 
 ##################
@@ -39,7 +44,7 @@ cd $grid/
 mkdir -p install/build-Nc4
 cd install/build-Nc4
 
-../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64  --enable-simd=GPU --enable-Nc=4 --disable-fermion-reps --with-lime=$prefix/lime-1.3.2 --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++14 -I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lamdhip64  -g -ltcmalloc -lmpi_gtl_hsa" HIPFLAGS="--amdgpu-target=gfx90a"
+../../Grid/configure --enable-comms=mpi-auto --enable-unified=no --enable-shm=nvlink --enable-accelerator=hip  --enable-gen-simd-width=64  --enable-simd=GPU --enable-Nc=4 --disable-fermion-reps --with-lime=$prefix/lime-1.3.2 --prefix=$prefix CXX=hipcc MPICXX=mpicxx CXXFLAGS="-fPIC -I/opt/rocm-5.2.0/include/ -std=c++17 -I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lamdhip64  -g -ltcmalloc -lmpi_gtl_hsa" HIPFLAGS="--amdgpu-target=gfx90a"
 
 make -j 14 2>&1 | tee op_grid.out
 make install
--- build_GRID/grid_tioga/setup_env.sh
+++ build_GRID/grid_tioga/setup_env.sh
@@ -1,10 +1,11 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/mpich/8.1.21/gtl/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/mpich/8.1.26/gtl/lib/
 
 module load PrgEnv-gnu
 module load rocm/5.2.0
-module load cray-mpich/8.1.17
+module load cray-mpich/8.1.26
 #module load gperftools
 #module load gmp
-module load cray-fftw
-module load craype-accel-amd-gfx90a
+module load craype-x86-rome
+#module load craype-accel-amd-gfx90a
+module load cray-fftw/3.3.8.11
 
